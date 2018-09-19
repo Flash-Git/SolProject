63ef9c904e3c9dba99b1e67df07194c37198b6b6
Flash <flash.jaq@gmail.com>
2018-09-19 12:49:51 +0100

    Attempted to continue testing

--- test/TestCryptoCookieMonsters.sol
+++ test/TestCryptoCookieMonsters.sol
@@ -9,9 +9,14 @@ contract TestCryptoCookieMonsters {
 	function testInitialVars() public {
 		CryptoCookieMonsters inst = CryptoCookieMonsters(DeployedAddresses.CryptoCookieMonsters());
 
-		uint initialCounter = 0;
+		uint expectedInitialCounter = 0;
+		//address expectedOwner = "0xa6a9a983a08250d82f6d43c1e8a00c38db17ef8c";//What is the call to get Truffle deployment addresses
 
-		Assert.equal(inst.counter(), initialCounter, "Issue with initial counter value");
+		//CookieMonster[] expectedMonsters = [];
+
+		Assert.equal(inst.counter(), expectedInitialCounter, "Issue with initial counter value");
+		//Assert.equal(inst.owner(), expectedOwner, "Issue with owner address");
+		//Assert.equal(inst.monsters(), expectedArray, "Issue with monster array");//How do you use structs?
 	}
 
 }
